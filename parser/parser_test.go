a1501618b245d6595114ce6351001d2b654a69e4
dumiqo <konstantin.kim.mail@gmail.com>
2023-10-12 15:35:47 +0300

    add integer parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -141,3 +141,30 @@ func TestIdentifierExpression(t *testing.T) {
 		t.Fatalf("ident.TokenLiteral not %s. got=%s", "foobar", ident.Value)
 	}
 }
+
+func TestIntegerLiteralExpression(t *testing.T) {
+	input := "5;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program hs not enough statements, got=%d", len(program.Statements))
+	}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements is not ast.ExpressionStatement, got=%T", program.Statements[0])
+	}
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.IntegerLiteral, got=%T", stmt.Expression)
+	}
+	if literal.Value != 5 {
+		t.Fatalf("literal.Value not %d, got=%d", 5, literal.Value)
+	}
+	if literal.TokenLiteral() != "5" {
+		t.Fatalf("literal.TokenLiteral not %s, got=%s", "5", literal.TokenLiteral())
+	}
+}
